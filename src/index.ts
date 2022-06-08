68615a9da64db7e3013036f0021f7dae72f88dcd
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-06-08 18:34:16 -0300

    Add types on the fucntion return and parameters

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,7 @@ interface ResponseTodo {
   
 }
 
-async function req(): Promise<void> {
-  const response = await axios.get(`${baseUrl}/todos/1`) as ResponseTodo;
+function printData(response: ResponseTodo): ResponseTodo {
   const { userId } = response.data;
   const { id } = response.data;
   const { title } = response.data;
@@ -25,6 +24,13 @@ async function req(): Promise<void> {
     TITLE: ${title}
     COMPLETED: ${completed}
   `)
+
+  return response;
+}
+
+async function req(): Promise<ResponseTodo> {
+  const response = await axios.get(`${baseUrl}/todos/1`) as ResponseTodo;
+  return printData(response)
 }
 
 req()
