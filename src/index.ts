ad7a0ae5baf8d42db8b0845d3c52faa87f6f442c
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-06-08 22:39:42 -0300

    create a function of convert decimal number to an integer number

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,32 @@ interface ResponseTodo {
     title: string;
     completed: boolean;
   };
-  
 }
+
+let numberInt: number;
+
+/**
+ * Converts a decimal number to an integer number.
+ * @param floatNumber Is a any decimal value.
+ * The number to be converted from decimal to integer and later returned.
+ * @param round Is a optional parameter for orientation of convert: upper, lower or truncate.
+ * If the 'round' not setted, the function returns the most close integer.
+ */
+function convertFloatToInt(floatNumber: number, round?: string):number {
+  switch(round) {
+    case 'upper':
+      return Math.ceil(floatNumber);
+    case 'lower':
+      return Math.floor(floatNumber);
+    case 'truncate':
+      return Math.trunc(floatNumber);
+    default:
+      return Math.round(floatNumber);
+  }
+}
+
+numberInt = convertFloatToInt(2.9)
+console.log('PARSE INT ------------------> ', numberInt)
 
 function printData(response: ResponseTodo): ResponseTodo {
   const { userId } = response.data;
